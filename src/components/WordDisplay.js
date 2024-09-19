ea0c9910e55b91112f756e1ba1aaeae69d887e9b
enloreal <arozarg@mail.ru>
2024-09-19 21:51:29 +0300

    updated design

--- src/components/WordDisplay.js
+++ src/components/WordDisplay.js
@@ -4,7 +4,7 @@ import styles from '../styles/PracticePage.module.css'
 const WordDisplay = ({ word, translation }) => {
   return (
     <div className={styles.pageInner}>
-      <h2>{word}</h2>
+      <h2 className={styles.jap}>{word}</h2>
       <p>({translation})</p>
     </div>  
   );
